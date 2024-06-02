0874b9cf5136e3f4c12543385bb8d819f42bf48c
minh1 <minh1@26.172.77.70>
2024-06-03 00:06:06 +0700

    User List

---
+++ src/java/controller/admin/ListUserServlet.java
@@ -0,0 +1,101 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package controller.admin;
+
+import dal.QuestionDAO;
+import dal.UserDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
+import model.QuizQuestion;
+import model.User;
+
+/**
+ *
+ * @author minh1
+ */
+public class ListUserServlet extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ListUserServlet</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ListUserServlet at " + request.getContextPath () + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    } 
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+       String indexPage =request.getParameter("index");
+      if(indexPage == null){
+          indexPage="1";
+      }
+      int index = Integer.parseInt(indexPage);
+        UserDAO dao = new UserDAO();
+       
+       int count = dao.getTotalUser();
+        int endPage =   count/5;
+        if(count % 5 != 0){
+            endPage++; 
+        }
+        List<User> list = dao.pagingUser(index);
+       request.setAttribute("listS",list );
+        request.setAttribute("endP", endPage);
+        request.getRequestDispatcher("list-user.jsp").forward(request, response);
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- src/java/dal/UserDAO.java
+++ src/java/dal/UserDAO.java
@@ -1,17 +1,21 @@
 package dal;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 import model.User;
+import java.sql.Date;
 
 /**
  *
  * @author Admin
  */
 public class UserDAO extends DBContext {
-
+     Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
     /**
      * @param args the command line arguments
      */
@@ -41,7 +45,52 @@ public class UserDAO extends DBContext {
         }
         return list;
     }
+    public List<User> pagingUser(int index) {
+        List<User> list = new ArrayList<>();
+        
+        String query = "select * from Users \n"
+                + "order by UserID\n"
+                + "OFFSET ? Rows fetch next 5 rows only ;";
+        try {
+            PreparedStatement pst = connection.prepareStatement(query);
+              pst.setInt(1, (index - 1) * 5);
+          ResultSet rs = pst.executeQuery();
+            while (rs.next()) {
+                User user = new User(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDate(4),
+                        rs.getString(5),
+                        rs.getString(6),
+                        rs.getString(7),
+                        rs.getString(8),
+                        rs.getBoolean(9),
+                        rs.getInt(10),
+                        rs.getString(11),
+                        rs.getDate(12));
+                list.add(user);
+                
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
+    public int getTotalUser() {
+        String query = "select count (*) from Users";
+        try {
+            conn = new DBContext().connection;
+            ps = conn.prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                return rs.getInt(1);
+            }
 
+        } catch (Exception e) {
+        }
+
+        return 0;
+    }
+    
     public static void main(String args[]) {
         // TODO code application logic here
         UserDAO udao = new UserDAO();
