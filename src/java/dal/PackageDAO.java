f05eeec0f19e6f4ace416f917cb14d9e658d2dad
quanNMHE170463 <106498637+quanNMHE170463@users.noreply.github.com>
2024-06-17 16:00:42 +0700

    Update

--- src/java/dal/PackageDAO.java
+++ src/java/dal/PackageDAO.java
@@ -13,8 +13,6 @@ import model.Packages;
  */
 public class PackageDAO extends DBContext {
 
-    /**
-     */
     public List<Packages> getAllPackage() {
         List<Packages> list = new ArrayList<>();
 
@@ -59,7 +57,7 @@ public class PackageDAO extends DBContext {
             pst.setInt(5, p.getDuration());
             pst.setString(6, p.getStatus());
             pst.executeUpdate();
-        } catch (Exception e) {
+        } catch (SQLException e) {
             e.getStackTrace();
         }
     }
@@ -119,6 +117,7 @@ public class PackageDAO extends DBContext {
             pst.setInt(7, p.getId());
             pst.executeUpdate();
         } catch (Exception e) {
+            e.getStackTrace();
         }
     }
 
@@ -131,6 +130,7 @@ public class PackageDAO extends DBContext {
             pst.setInt(2, id);
             pst.executeUpdate();
         } catch (Exception e) {
+            e.getStackTrace();
         }
     }
 
