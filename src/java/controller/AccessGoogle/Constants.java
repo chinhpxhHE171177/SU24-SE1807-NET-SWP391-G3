f43cd138a8b155cfc8c49fad1e62d8108f8a93d1
quanNMHE170463 <106498637+quanNMHE170463@users.noreply.github.com>
2024-06-20 09:36:51 +0700

    Update

--- src/java/controller/AccessGoogle/Constants.java
+++ src/java/controller/AccessGoogle/Constants.java
@@ -11,7 +11,7 @@ package controller.AccessGoogle;
 public class Constants {
   public static String GOOGLE_CLIENT_ID = "237610735806-huhmj63f517dinbonapn00fnna7e4o1b.apps.googleusercontent.com";
   public static String GOOGLE_CLIENT_SECRET = "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";
-  public static String GOOGLE_REDIRECT_URI = "http://localhost:9999/Quizz/login-google";
+  public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/Quizz/login-google";
   public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
   public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
   public static String GOOGLE_GRANT_TYPE = "authorization_code";
