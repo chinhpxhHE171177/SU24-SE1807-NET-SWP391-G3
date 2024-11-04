deb6a9ce8ed0dec77fc01906d22cd0ede27e5fe3
chinhpxHE171177 <169163635+chinhpxhHE171177@users.noreply.github.com>
2024-11-04 07:06:17 +0700

    newwwww

---
+++ libraries/main/java/com/example/qrcode/HistoryAdapter.java
@@ -0,0 +1,114 @@
+package com.example.qrcode;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.Holder> {
+
+    private Context context;
+    private ArrayList<Device_History> arrayList;
+    private DatabaseHelper databaseHelper;
+    private Toast mToast;
+
+    public HistoryAdapter(Context context, ArrayList<Device_History> arrayList) {
+        this.context = context;
+        this.arrayList = arrayList;
+        this.databaseHelper = new DatabaseHelper(context);
+    }
+
+    @NonNull
+    @Override
+    public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.row_history, parent, false);
+        return new Holder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final Holder holder, int position) {
+        Device_History model = arrayList.get(position);
+//        holder.txt_Id.setText(String.valueOf(model.getId()));
+        holder.txt_Code.setText("Code: " + model.getCode());
+        holder.txt_Name.setText("Name: " + model.getName());
+        holder.txt_Stage.setText("Stage: " + model.getStage());
+        holder.txt_Issue.setText("Issue: " + model.getIssue());
+        holder.txt_StartTime.setText("BD: "  + model.getStartTime());
+        holder.txt_EndTime.setText("KT: " + model.getEndTime());
+        holder.txt_Duration.setText("Duration: " + model.getTotalTime());
+        holder.txt_TypeName.setText("Type: " + model.getTypeName());
+        //holder.txt_ErrorCount.setText("Errors: " + model.getRecordCount());
+
+        holder.img_Delete.setOnClickListener(v -> {
+            int currentPosition = holder.getAdapterPosition();
+            if (currentPosition != RecyclerView.NO_POSITION) {
+                new AlertDialog.Builder(context)
+                        .setTitle("Confirm Deletion")
+                        .setMessage("Are you sure want to delete this history record?")
+                        .setPositiveButton("OK", (dialog, which) -> {
+                            // Nếu nhấn "OK", thực hiện xóa
+                            boolean deleted = databaseHelper.deleteHistoryBId(model.getId());
+                            if (deleted) {
+                                arrayList.remove(currentPosition);
+                                notifyItemRemoved(currentPosition);
+                                notifyItemRangeChanged(currentPosition, arrayList.size());
+                                showToast("History record deleted");
+                            } else {
+                                showToast("Failed to delete");
+                            }
+                        })
+                        .setNegativeButton("Cancel", (dialog, which) -> {
+                            dialog.dismiss();
+                        })
+                        .show();
+            }
+        });
+    }
+
+
+    private void showToast(String message) {
+        if (mToast != null) {
+            mToast.cancel();
+        }
+        mToast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
+        mToast.show();
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+
+    public static class Holder extends RecyclerView.ViewHolder {
+        private ImageView img_photo;
+        private ImageButton img_Delete;
+        private TextView txt_Id, txt_Code, txt_Name, txt_Stage, txt_Issue, txt_ErrorCount, txt_TypeName, txt_StartTime, txt_EndTime, txt_Duration;
+
+        public Holder(@NonNull View itemView) {
+            super(itemView);
+            img_photo = itemView.findViewById(R.id.image_shop);
+//            txt_Id = itemView.findViewById(R.id.txt_Id);
+            txt_Code = itemView.findViewById(R.id.txt_Code);
+            txt_Name = itemView.findViewById(R.id.txt_Name);
+            txt_Stage = itemView.findViewById(R.id.txt_Stage);
+            txt_Issue = itemView.findViewById(R.id.txt_Issue);
+            //txt_ErrorCount = itemView.findViewById(R.id.txt_Error_Count);
+            txt_StartTime = itemView.findViewById(R.id.txt_Start_Time);
+            txt_EndTime = itemView.findViewById(R.id.txt_End_Time);
+            txt_Duration = itemView.findViewById(R.id.txt_Duration);
+            txt_TypeName = itemView.findViewById(R.id.txt_TypeName);
+            img_Delete = itemView.findViewById(R.id.btn_Delete_Row);
+        }
+    }
+}
---
+++ libraries/main/java/com/example/qrcode/UploadDataTask.java
@@ -0,0 +1,89 @@
+package com.example.qrcode;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class UploadDataTask extends AsyncTask<ArrayList<Device_History>, Void, Boolean> {
+    private Context context;
+    private OnUploadCompleteListener listener;
+    private boolean isUploading = false;
+
+    // Define a listener interface
+    public interface OnUploadCompleteListener {
+        void onUploadComplete(Boolean success);
+    }
+
+    // Constructor now accepts a listener
+    public UploadDataTask(Context context, OnUploadCompleteListener listener) {
+        this.context = context;
+        this.listener = listener;
+    }
+
+    @Override
+    protected Boolean doInBackground(ArrayList<Device_History>... params) {
+        ArrayList<Device_History> deviceList = params[0];
+        try {
+            URL url = new URL("http://192.168.1.5:8080/ssmqrcode/uploadData");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setDoOutput(true);
+            connection.connect();
+
+            JSONArray jsonArray = new JSONArray();
+            for (Device_History device : deviceList) {
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("id", device.getId());
+                jsonObject.put("code", device.getCode());
+                jsonObject.put("name", device.getName());
+                jsonObject.put("stage", device.getStage());
+                jsonObject.put("issue", device.getIssue());
+                jsonObject.put("startTime", device.getStartTime());
+                jsonObject.put("endTime", device.getEndTime());
+                jsonObject.put("duration", device.getTotalTime());
+                jsonObject.put("typeName", device.getTypeName());
+                jsonArray.put(jsonObject);
+            }
+
+            OutputStream os = connection.getOutputStream();
+            os.write(jsonArray.toString().getBytes());
+            os.close();
+
+            Log.d("UploadDataTask", "JSON to send: " + jsonArray.toString());
+
+            int responseCode = connection.getResponseCode();
+            return responseCode == HttpURLConnection.HTTP_OK;
+
+        } catch (JSONException | IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    protected void onPostExecute(Boolean success) {
+        if (success) {
+            Toast.makeText(context, "Data upload successful", Toast.LENGTH_SHORT).show();
+            if (listener != null) {
+                listener.onUploadComplete(true);
+            }
+        } else {
+            Toast.makeText(context, "Data upload failed", Toast.LENGTH_SHORT).show();
+            if (listener != null) {
+                listener.onUploadComplete(false);
+            }
+        }
+    }
+}
